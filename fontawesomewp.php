72c7b0df6df7a8ef7b407f9110874c0eed19ae36
Rolands Umbrovskis <webapp@mediabox.lv>
2013-04-03 20:10:17 +0300

    init

---
+++ fontawesomewp.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Plugin Name: Font Awesome for WordPress
+ * Plugin URI: http://mediabox.lv/#utm_source=wordpress&utm_medium=plugin&utm_campaign=draugiemsaypoga_widget&utm_content=v01
+ * Description: WordPress spraudnis "Draugiem.lv/say" pogas pievienošana satura apakšā ar integrētu bit.ly saišu saīsināšanu
+ * Version: 1.0.0
+ * Requires at least: 2.6
+ * Author: Rolands Umbrovskis
+ * Author URI: http://umbrovskis.com
+ * License: GPL
+ */
+
+/**
+ * Add function to widgets_init that will load meblog fontawesomewp.
+ * @since 1.0.0
+ */
+// add_action( 'widgets_init', 'meblogfrypepage_load_widgets' );
+
+define('FAMWPV','1.0.0'); // version @since 1.0.0
+define('FAMWPF','fontawesomewp'); // folder @since 1.0.0
+define('FAMWPL',dirname(__FILE__)); // location @since 1.0.0
+define('FAMWPCSS',plugins_url(FAMWPF).'/css'); // CSS location @since 1.0.0
+define('FAMWPFONTF',plugins_url(FAMWPF).'/font'); // font location @since 1.0.0
+
+/**
+* Loading language files
+* @version 1.0.0
+* @since 1.0.0
+*/
+
+load_plugin_textdomain( 'fontawesomewp', false, dirname(plugin_basename(__FILE__)) . '/lang/' ); 
+
+
+/**
+* Meta info for Plugin list page
+* @version 1.0.0
+* @todo Plugin page http://mediabox.lv/ OR http://simplemediacode.com , Support Forum
+*/
+function fontawesomewp_set_plugin_meta($links, $file) {
+	$plugin = plugin_basename(__FILE__);
+	if ($file == $plugin) {
+		return array_merge( $links, array( 
+			'<a href="http://umbrovskis.com/ziedo/">' . __('Donate','fontawesomewp') . '</a>'
+		));
+	}
+	return $links;
+}
+add_filter( 'plugin_row_meta', 'fontawesomewp_set_plugin_meta', 10, 2 );
+
+
+function fontawesomewp_init() {
+	if( !is_admin()){
+		global $wp_styles;
+		// STYLES
+		wp_register_style('font-awesome', FAMWPCSS.'/font-awesome.css', array(), '2.0', 'all');
+		wp_enqueue_style('font-awesome');
+		wp_register_style('font-awesome-social', FAMWPCSS.'/font-awesome-social.css', array('font-awesome'), '2.0', 'all');
+		wp_enqueue_style('font-awesome-social');
+		wp_register_style('font-awesome-corp', FAMWPCSS.'/font-awesome-corp', array('font-awesome'), '2.0', 'all');
+		wp_enqueue_style('font-awesome-corp');
+		wp_register_style('font-awesome-ext', FAMWPCSS.'/font-awesome-ext.css', array('font-awesome'), '2.0', 'all');
+		wp_enqueue_style('font-awesome-ext');
+
+		wp_register_style('font-awesome-ie7', FAMWPCSS.'/font-awesome-ie7.min.css', array('font-awesome'), '2.0', 'all');
+		wp_enqueue_style('font-awesome-ie7');
+			$wp_styles->add_data( 'font-awesome-ie7', 'conditional', 'IE 7' );
+			
+		wp_register_style('font-awesome-more-ie7', FAMWPCSS.'/font-awesome-more-ie7.min.css', array('font-awesome'), '2.0', 'all');
+		wp_enqueue_style('font-awesome-more-ie7');
+			$wp_styles->add_data( 'font-awesome-more-ie7', 'conditional', 'IE 7' );
+
+	}
+
+}    
+
+add_action('init', 'fontawesomewp_init');
+
---
+++ readme.txt
@@ -0,0 +1,70 @@
+﻿=== Font Awesome for WordPress  ===
+Contributors: rolandinsh
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=Z4ALL9WUMY3CL&lc=LV&item_name=Umbrovskis%2e%20WordPress%20plugins&item_number=002&currency_code=EUR&bn=PP%2dDonationsBF%3abtn_donate_SM%2egif%3aNonHosted
+Tags: font, iconic font, theme enhancement, web fonts, webfonts, css font embed, font conversion, font embed, type, typography, fontface, font type, fonttype, WPMU, WordPress MU, WPMS, WP Multisite, Multisite, WordPress MS, WordPress Multisite, buddypress, bp, fonts, CSS,
+Requires at least: 2.6
+Tested up to: 3.5.1
+Stable tag: 1.0.0
+License: GPL, SimpleMediaCode
+License URI: http://simplemediacode.com/license/gpl/
+
+Font Awesome (more) for WordPress - The iconic font for any theme enhancement.
+
+== Description ==
+
+Font Awesome (more) for WordPress - The iconic font for any theme enhancement.
+
+The full suite of pictographic icons, examples, and documentation can be found at: [Font Awesome More](http://gregoryloucas.github.com/Font-Awesome-More/)
+
+= Features = 
+
+* Over 300 icons: In a single collection, Font Awesome is a pictographic language of web-related actions. Font Awesome More delivers the extras you were looking for. 
+* CSS control: Easily style icon color, size, shadow, and anything that's possible with CSS. 
+* Infinite scalability: Scalable vector graphics means every icon looks awesome at any size. 
+* Free, as in Beer: Font Awesome More is completely free for commercial use. Check out the license.  
+* IE7 Support: Font Awesome More supports IE7. If you need it, you have my condolences. 
+* Perfect on Retina Displays: Font Awesome More icons are vectors, which mean they're gorgeous on high-resolution displays.
+* And now for ***WordPreess***. 
+
+== Changelog ==
+
+= 1.0.0 =
+
+* Initial release
+
+== More info ==
+
+* [Font Awesome for WordPress plugin site](http://simplemediacode.com/wordpress-pugins/font-awesome-for-wordpress/)
+* [Font Awesome More info](http://gregoryloucas.github.com/Font-Awesome-More/)
+
+== Installation ==
+
+Installation:
+
+* Download the plugin.
+* Copy the `fontawesomewp` folder to the plugins directory of your blog.
+* Enable the plugin in your admin panel.
+* An options panel will appear in admin/dashboard left sidebar.
+* enjoy
+
+Use in HTML area like `<i class="icon-user"></i>`
+
+= Documentation =
+
+On page. Coming soon as wiki :)
+
+== Frequently Asked Questions ==
+
+There are no questions for now! !
+
+== Upgrade Notice ==
+
+= 1.0.0 =
+
+Initial release
+
+== Screenshots ==
+
+1. live on http://simplemediacode.com/wordpress-pugins/font-awesome-for-wordpress/
+1. source
+
